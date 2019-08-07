2f957ed05e142582a48612a37fec10418a5eb504
dieman95 <diego.manzanas.lopez@vanderbilt.edu>
2019-08-07 17:14:22 -0500

    Added and modified simulations for mariner and Lcontainer

--- NNCS/mariner-uuv/run_all.m
+++ NNCS/mariner-uuv/run_all.m
@@ -1,4 +1,4 @@
-%% Run all simulations
+%% Run all mpossible mariner simulations
 % For now, it runs all simulations (simple and sonar) with the PID controller and the
 % neural network controller. All simulations generate different pipelines
 
@@ -7,7 +7,7 @@
 % Simulation 1 - Simple with PID controller
 fi = figure;
 'Simulation 1'
-sim1 = follow_pipeline_simple(175,25,@LContainer,9,4,5,6,10000,0.1,[3 6]);
+sim1 = follow_pipeline_simple(1.98,25,@mariner_uuv,7,4,5,6,10000,0.1,[3 6]);
 'Press any key for moving into next simulation'
 title('Simulation 1 - Waypoints and ground truth (PID)');
 xlabel('x positon (m)');
@@ -19,7 +19,7 @@ clc;
 % Simulation 2 - Simulated sonar with PID controller
 fi = figure;
 'Simulation 2'
-sim2 = follow_pipeline_sonar(175,25,@LContainer,9,4,5,6,10000,0.1,[3 6]);
+sim2 = follow_pipeline_sonar(1.98,25,@mariner_uuv,7,4,5,6,10000,0.1,[3 6]);
 'Press any key for moving into next simulation'
 title('Simulation 2 - Simulated sonar and ground truth (PID)');
 xlabel('x positon (m)');
@@ -31,7 +31,7 @@ clc;
 % Simulation 3 - Simple with NN controller
 fi = figure;
 'Simulation 3'
-sim3 = follow_pipeline_simple_NN(175,25,@LContainer,9,4,5,6,10000,0.1,[3 6],'controller_mariner_3in_net.mat');
+sim3 = follow_pipeline_simple_NN(1.98,25,@mariner_uuv,7,4,5,6,10000,0.1,[3 6],'controller_mariner_3in_net.mat');
 'Press any key for moving into next simulation'
 title('Simulation 3 - Waypoints and ground truth (NN)');
 xlabel('x positon (m)');
@@ -43,9 +43,10 @@ clc;
 % Simulation 4 - Simulated sonar with NN controller
 fi = figure;
 'Simulation 4'
-sim4 = follow_pipeline_sonar_NN(175,25,@LContainer,9,4,5,6,10000,0.1,[3 6],'controller_mariner_3in_net.mat');
+sim4 = follow_pipeline_sonar_NN(1.98,25,@mariner_uuv,7,4,5,6,10000,0.1,[3 6],'controller_mariner_3in_net.mat');
 title('Simulation 4 - Simulated sonar and ground truth (NN)');
 xlabel('x positon (m)');
 ylabel('y position (m)');
 saveas(fi,'figures/sim4','png');
 'End of simulations'
+
